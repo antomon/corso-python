f85394fe4646f2fc2117825111ec637640243d96
antomon <antonio.montano@gmail.com>
2024-05-30 12:51:55 +0200

    up

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-nome = "Python"
-definizione = "Linguaggio"
-
-f_stringa_multi_linea = f'''Questo è un esempio
-di f-stringa multi-linea
-in {definizione.lower() + ' ' + nome}''' # <2>
-
-print(f_stringa_multi_linea)
+x= 0
+assert x > 0, "x deve essere positivo"
