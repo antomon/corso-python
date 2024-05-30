a2a767599bb0171df22446aa4661bf8c0242fd1a
antomon <antonio.montano@gmail.com>
2024-05-30 12:02:00 +0200

    up

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-print(type(...))  # <class 'ellipsis'> # <1>
+nome = "Python"
+definizione = "Linguaggio"
 
-def funzione_da_completare():
-  ... # <2>
+f_stringa_multi_linea = f'''Questo è un esempio
+di f-stringa multi-linea
+in {definizione.lower() + ' ' + nome}''' # <2>
 
-class ClasseEsempio:
-  def metodo_da_completare(self):
-    ...
-
-from typing import Callable
-
-def funzione_variadica(func: Callable[..., int]): # <3>
-  pass
-
-import numpy as np
-
-array = np.array([[[1, 2, 3],    [4, 5, 6]], 
-                  [[7, 8, 9], [10, 11, 12]]]) 
-
-print(array[..., 1]) # <4>
+print(f_stringa_multi_linea)
