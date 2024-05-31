1a0f80388be2fe8853e97b04dfcabbbdc47aa4ac
antomon <antonio.montano@gmail.com>
2024-05-31 17:40:11 +0200

    up

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-def pari_o_dispari(n):
-    assert isinstance(n, int), \
-      "Errore: l'input deve essere un numero intero"
-    
-    if n % 2 == 0:
-        return "Pari"
-    else:
-        return "Dispari"
-      
-print(pari_o_dispari(3))
+print(type(...))  # <class 'ellipsis'> # <1>
+
+def funzione_da_completare():
+  ... # <2>
+
+class ClasseEsempio:
+  def metodo_da_completare(self):
+    ...
+
+from typing import Callable
+
+def funzione_variadica(func: Callable[..., int]): # <3>
+  pass
+
+import numpy as np
+
+array = np.array([[[1, 2, 3],    [4, 5, 6]], 
+                  [[7, 8, 9], [10, 11, 12]]]) 
+
+print(array[..., 1]) # <4>
