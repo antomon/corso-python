7ef1dff9d93ab0bf73ac630dc2d4d19afc3aef02
antomon <antonio.montano@gmail.com>
2024-06-05 10:16:03 +0200

    up

--- test.py
+++ test.py
@@ -1,13 +1,30 @@
-# Test di diversità
-x = 4 != 4.
-print(x)
 
-x = 0 != (1 - 1)
-print(x)
 
-x = 0 or (0 + 0)
-print(x)
 
-x = 0 or 2
-print(x)
 
+s = "Hello" + ' ' + 'World!'
+print(s)
+
+ss = s
+
+ss *= 2
+print(ss)
+print(s)
+
+b = 'el' in s
+print(b)
+
+b = 'oo' not in s
+print(b)
+
+b = "Ciao Mondo!" < s
+print(b)
+
+s_ = ss[:len(s)]
+print(s)
+
+l = len(ss)
+print(l)
+
+s_ = ss[:int(len(ss) / 2)]
+print(s_)
