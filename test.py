5d4174ef2b057533dce41e094b0e28acf0459a46
antomon <antonio.montano@gmail.com>
2024-06-14 11:39:03 +0200

    up

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
+def analizza_tupla(tupla):  # <1>
+  match tupla:  # <2>
+    case (x, (y, z)):  # <3>
+      print(f"Primo elemento: {x}, Secondo elemento: ({y}, {z})")  # <4>
+        
+    case (x, y):  # <5>
+      print(f"Primo elemento: {x}, Secondo elemento: {y}")  # <6>
 
+    case _:  # <7>
+      print("Pattern non riconosciuto")  # <8>
 
-
-
-def rimuovi_duplicati(lista):
-  return sorted(dict.fromkeys(lista))
-
-# Esempio di utilizzo
-lista = [4, 2, 2, 3, 1, 4, 5]
-
-print(rimuovi_duplicati(lista)) 
+# Esempi di utilizzo della funzione
+analizza_tupla((1, (2, 3)))  # <9>
+analizza_tupla((1, 4))  # <10>
+analizza_tupla((1, 2, 3))  # <11>
