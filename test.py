dccccc24ca36eb817070876ef4e83b5e4d5af631
antomon <antonio.montano@gmail.com>
2024-06-16 00:18:24 +0200

    up

--- test.py
+++
@@ -1,6 +0,0 @@
-def somma_numeri(numeri: list[int]) -> int:  # <1>
-    """Restituisce la somma di una lista di numeri interi."""  # <2>
-    return sum(numeri)  # <3>
-
-print(somma_numeri([1, 2, 3, 4]))  # <4>
-print(somma_numeri.__annotations__)  # <5>
