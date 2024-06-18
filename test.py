8c6297411eaa7e6f90504a990a43a6f9a9018169
antomon <antonio.montano@gmail.com>
2024-06-19 00:21:58 +0200

    up

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-from flask import Flask, render_template, request  # <1>
+_count = 0  # <1>
 
-app = Flask(__name__)  # <2>
+def contatore():  # <2>
 
-@app.route('/', methods=['GET', 'POST'])  # <3>
-def home():
-    if request.method == 'POST':  # <4>
-        indirizzo = request.form['indirizzo']  # <5>
-        visualizzazione = request.form['visualizzazione']  # <6>
-        return render_template('mappa.html', 
-                               indirizzo=indirizzo, 
-                               visualizzazione=visualizzazione)  # <7>
-    return render_template('index.html')  # <8>
 
-if __name__ == '__main__':
-    app.run(debug=True)  # <9>
+  _count += 1  # <4>
+
+  return _count  # <5>
+
+print(contatore())  # <6>
+print(contatore())  # <7>
